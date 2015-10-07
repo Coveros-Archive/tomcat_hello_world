1f42cc036f0ff0a27f7f40badf7876db4fdd2bf3
devin909 <devin.lee@coveros.com>
2015-10-07 15:17:05 -0400

    good code

--- src/main/java/com/coveros/hello_world/RandomEvenNumberGenerator.java
+++ src/main/java/com/coveros/hello_world/RandomEvenNumberGenerator.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class RandomEvenNumberGenerator {
 	public static int generateRandomEven(){
 		Random randomGenerator = new Random();
-		asdfasldfkjsldkfj;
 		int result = randomGenerator.nextInt();
 		if (result%2!=0){
 			result++;
